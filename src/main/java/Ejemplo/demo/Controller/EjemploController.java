7822f94bb62fe6f6393820a33f2c47d9d0ff210f
Angelo Sotomayor <assv.9330@gmail.com>
2022-04-19 19:57:42 -0600

    Proyecto finalizado

---
+++ src/main/java/Ejemplo/demo/Controller/EjemploController.java
@@ -0,0 +1,16 @@
+
+package Ejemplo.demo.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class EjemploController {
+    
+    @GetMapping("/")
+    public String Ejemplo(){
+        return "/Ejemplo";
+    }
+}
